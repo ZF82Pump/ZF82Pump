e3f256986929d90f848295c9c92c9c7cb267e744
ZF82Pump <ethanet82@gmail.com>
2026-01-12 11:01:58 -0500

    Delete supabaseClient.js to satisfy error

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-export const supabase = createClient(
-  "https://vycwkgpdivxazjgsrgiq.supabase.co",
-  "sb_publishable_aAjs8tF_8c8sB30ekxSXZQ_KxaBMBTE"
-);
-
