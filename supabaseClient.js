8746ee4f5e2e8895fa02f0deef395585ae30935c
ZF82Pump <ethanet82@gmail.com>
2026-01-12 10:54:54 -0500

    Update supabaseClient.js with url and token

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 export const supabase = createClient(
-  "YOUR_SUPABASE_URL_HERE",
-  "YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY_HERE"
+  "https://vycwkgpdivxazjgsrgiq.supabase.co",
+  "sb_publishable_aAjs8tF_8c8sB30ekxSXZQ_KxaBMBTE"
 );
+
